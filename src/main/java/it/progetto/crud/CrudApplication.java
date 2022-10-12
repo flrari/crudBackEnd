e7dcd41308fefbd61d53f959b67473de78109dcd
Arianna Floris <flrari@hotmail.com>
2022-10-12 12:06:22 +0200

    First commit

---
+++ src/main/java/it/progetto/crud/CrudApplication.java
@@ -0,0 +1,13 @@
+package it.progetto.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication //Java Notations, informazioni che andiamo a specificare nelle singole classi
+public class CrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudApplication.class, args); //metodo che fa partire tutta l'application
+	}
+
+}
