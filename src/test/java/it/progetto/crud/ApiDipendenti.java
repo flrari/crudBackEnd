4b6daa8a57220017304a7f71e1069222f058b0a2
Arianna Floris <flrari@hotmail.com>
2022-10-17 10:40:04 +0200

    Add new features

--- src/test/java/it/progetto/crud/ApiDipendenti.java
+++ src/test/java/it/progetto/crud/ApiDipendenti.java
@@ -3,10 +3,7 @@ package it.progetto.crud;
 import it.progetto.crud.model.Dipendente;
 import it.progetto.crud.service.DipendenteService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,6 +18,17 @@ public class ApiDipendenti {
     @GetMapping("/all")
     public List<Dipendente> getAllDipendente(){
         return dipendenteService.findAllDipendente();
+    }
+    
+    //@CrossOrigin(origins = "http://localhost:4200")
+    @GetMapping("/find/{id}")
+    public Dipendente getDipendenteById(@PathVariable("id") Long id){
+        return dipendenteService.findDipendenteById(id);
+    }
+
+    @PostMapping("/add")
+    public  Dipendente addDipendente(@RequestBody Dipendente dipendente){
+        return dipendenteService.addDipendente(dipendente);
     }
 
 }
--- src/test/java/it/progetto/crud/repository/DipendenteRepository.java
+++ src/test/java/it/progetto/crud/repository/DipendenteRepository.java
@@ -3,8 +3,6 @@ package it.progetto.crud.repository;
 import it.progetto.crud.model.Dipendente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface DipendenteRepository extends JpaRepository<Dipendente, Long> {
 
     Dipendente findDipendenteById(Long id);
